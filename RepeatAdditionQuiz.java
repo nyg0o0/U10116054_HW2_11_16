2042e911ae6bb84f28b00f04b375128ee801af6a
Zoe <yaya56790@gmail.com>
2016-03-05 13:36:34 +0800

    Update RepeatAdditionQuiz.java
    comment

--- RepeatAdditionQuiz.java
+++ RepeatAdditionQuiz.java
@@ -3,11 +3,7 @@
   Strudent ID: U10116054
   Strudent Name: Yu-Hsin Chen
   Assign Date: 3/7
-
-  Content:This is a program for bank account, it can show some informations of your account.
-  
--->  RepeatAdditionQuiz.java  ----- main program
-  
+  Content:This is a program for repeat addition quiz with arraylist.
 */
 
 // import java api
@@ -17,29 +13,48 @@ import java.util.ArrayList;
 
 // RepeatAdditionQuiz class
 public class RepeatAdditionQuiz {
+  
   //main method
   public static void main(String[] args){
+    
+    // creat array list
     ArrayList<Integer> answerList = new ArrayList<>();
+    
+    // generate two random integer
     int number1 = (int)(Math.random() * 10);
     int number2 = (int)(Math.random() * 10);
     
+    // scanner
     Scanner input = new Scanner(System.in);
 
+    // prompt the user to enter the answer
     System.out.print(
      "What is " + number1 + " + " + number2 + "? ");
      int answer = input.nextInt();
 
+
+    // if answer is wrong
     while (number1 + number2 != answer){
+      // if input answer is not in arraylist
       if (!answerList.contains(answer)){
+        // add to the arraylist
         answerList.add(answer);
       }
       else{
+        // the input answer is already be entered once 
         System.out.println("You already entered " + answer);
       }
+      
+      // prompt user to enter a answer agin
       System.out.print("Wrong answer. Try again. What is "
        + number1 + " + " + number2 + "? ");
+       // scanner the input answer
        answer = input.nextInt();
-    }
+    } // end of while
+    
+    // right answer
     System.out.println("You got it!");
-  }
-}
+    
+  } // end main method
+
+}  // end class
